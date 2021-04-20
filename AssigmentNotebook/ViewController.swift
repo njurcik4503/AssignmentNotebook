6af7808ae3caeab1d4fa911e680fa5a26bf462c7
natalie <njurcik4503@stu.d214.org>
2021-04-20 14:36:40 -0500

    Initial Commit

---
+++ AssigmentNotebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssigmentNotebook
+//
+//  Created by Natalie Jurcik on 4/20/21.
+//  Copyright © 2021 Natalie Jurcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
